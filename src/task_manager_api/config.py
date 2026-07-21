957d644cb0aeb8186b5ca3c3e0372769ec4cea7d
segfaultering <261435586+segfaultering@users.noreply.github.com>
2026-07-21 09:51:10 +0500

    Add config

--- src/task_manager_api/config.py
+++ src/task_manager_api/config.py
@@ -1,5 +1,12 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict 
+from pydantic import Field, PostgresDsn
+from dotenv import find_dotenv
 
 
 class Settings(BaseSettings):
+    migration_db_url: PostgresDsn = Field(validation_alias="MIGRATION_DATABASE_URL")
+    app_db_url: PostgresDsn = Field(validation_alias="APPLICATION_DATABASE_URL")
 
+    model_config = SettingsConfigDict(env_file=find_dotenv(".env"))
+
+settings = Settings()
