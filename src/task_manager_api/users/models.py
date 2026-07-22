d45d9c00a9fa173c77e5a2b79620a30b106f7c4d
segfaultering <261435586+segfaultering@users.noreply.github.com>
2026-07-22 12:54:57 +0500

    Finished user table

--- src/task_manager_api/users/models.py
+++ src/task_manager_api/users/models.py
@@ -1,8 +1,16 @@
+from datetime import datetime
+
 from sqlalchemy import (
+    func,
+    text,
     Identity,
     Integer,
     Text,
-    PrimaryKeyConstraint
+    Boolean,
+    DateTime,
+    PrimaryKeyConstraint,
+    UniqueConstraint,
+    CheckConstraint
 )
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -13,9 +21,28 @@ class User(Base):
 
     __tablename__="user"
      
-    id: Mapped[int] = mapped_column(Integer, Identity(start=1, increment=1))
-    username: Mapped[str] = mapped_column(Text, nullable=False)
-
+    id: Mapped[int] = mapped_column(
+        Integer, 
+        Identity(always=True, start=1, increment=1)
+    )
+    username: Mapped[str | None] = mapped_column(Text)
+    email: Mapped[str] = mapped_column(Text, nullable=False) 
+    hashed_password: Mapped[str] = mapped_column(Text, nullable=False)
+    admin: Mapped[bool] = mapped_column(
+        Boolean, 
+        server_default=text("FALSE"), 
+        nullable=False
+    )
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, 
+        server_default=func.current_timestamp(), 
+        nullable=False
+    )
+     
     __table_args__ = (
         PrimaryKeyConstraint("id", name="pk_user"),
+        UniqueConstraint("username", "email", name="uq_user_email_username"),
+        CheckConstraint("username", "username ~ ^[A-Za-z0-9._]+$", name="ck_users_valid_username"),
+        CheckConstraint("username", "length(username) >= 8"),
+        CheckConstraint("email", "email ~ ^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", name="ck_email_valid_email")
     )
