9c5d7ec873ad2d63aad051b05c022f0ca13c76ec
segfaultering <261435586+segfaultering@users.noreply.github.com>
2026-07-23 15:56:53 +0500

    Updated single GET response schema for users

--- src/task_manager_api/users/schemas.py
+++ src/task_manager_api/users/schemas.py
@@ -1,8 +1,23 @@
-from pydantic import BaseModel, Field
+from pydantic import (
+    BaseModel, 
+    Field, 
+    EmailStr,
+    StrictBool,
+    PositiveInt, 
+    NaiveDatetime
+)
 
 
 class UserCreate(BaseModel):
     username: str
 
 class UserResponse(BaseModel):
-    id: int = Field(gt=0)
+    id: PositiveInt
+    email: EmailStr
+    admin: StrictBool
+    created_at: NaiveDatetime
+    username: str | None = Field(
+        default=None,
+        min_length=8,
+        pattern="^[A-Za-z0-9_.]$"
+    )
